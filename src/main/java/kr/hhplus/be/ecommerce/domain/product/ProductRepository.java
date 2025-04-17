692d3b39870ecc08cfc1442949d997ecd8e67776
khs2973 <h1372@naver.com>
2025-04-18 02:52:36 +0900

    Feat: ProductRepository 추가

--- src/main/java/kr/hhplus/be/ecommerce/domain/product/ProductRepository.java
+++ src/main/java/kr/hhplus/be/ecommerce/domain/product/ProductRepository.java
@@ -2,11 +2,8 @@ package kr.hhplus.be.ecommerce.domain.product;
 
 import java.util.Optional;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
 public interface ProductRepository {
 	
-	Optional<Product> findByProductIdAndState(Integer productId, Integer productState);
+	Optional<Product> findByProductId(Integer productId);
 	
 }
