8de2f38d8712d736ce931bfde0af1ae5cf63d585
khs2973 <h1372@naver.com>
2025-04-18 06:12:59 +0900

    Fix: ErrorEnum 추가

--- src/main/java/kr/hhplus/be/ecommerce/interfaces/common/ErrorEnum.java
+++ src/main/java/kr/hhplus/be/ecommerce/interfaces/common/ErrorEnum.java
@@ -9,6 +9,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorEnum {
 
+	NOT_FOUND_COUPON(HttpStatus.BAD_REQUEST, "해당 쿠폰이 존재하지 않습니다."),
+	ALREADY_USED_COUPON(HttpStatus.BAD_REQUEST, "이미 사용한 쿠폰입니다."),
+	EXPIRED_COUPON(HttpStatus.BAD_REQUEST, "만료된 쿠폰입니다."),
 	NOT_ENOUGH_POINT(HttpStatus.BAD_REQUEST, "포인트가 부족합니다."),
 	NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "유효하지 않은 사용자입니다."),
 	CHARGE_POINT_MAX(HttpStatus.BAD_REQUEST, "최대 충전 금액 한도를 초과하셨습니다."),
