c297109007ec4faf9fd58a13c68770c9fe8edcfc
khs2973 <h1372@naver.com>
2025-04-24 01:52:10 +0900

    Fix: ErrorEnum 내용 추가

--- src/main/java/kr/hhplus/be/ecommerce/interfaces/common/ErrorEnum.java
+++ src/main/java/kr/hhplus/be/ecommerce/interfaces/common/ErrorEnum.java
@@ -9,14 +9,15 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorEnum {
 
-	NOT_FOUND_COUPON(HttpStatus.BAD_REQUEST, "해당 쿠폰이 존재하지 않습니다."),
-	ALREADY_USED_COUPON(HttpStatus.BAD_REQUEST, "이미 사용한 쿠폰입니다."),
-	EXPIRED_COUPON(HttpStatus.BAD_REQUEST, "만료된 쿠폰입니다."),
-	NOT_ENOUGH_POINT(HttpStatus.BAD_REQUEST, "포인트가 부족합니다."),
-	NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "유효하지 않은 사용자입니다."),
+	NOT_ENOUGH_PRODUCT(HttpStatus.BAD_REQUEST, "해당 상품의 재고가 부족합니다."),
+	NOT_FOUND_PRODUCT(HttpStatus.BAD_REQUEST, "해당 상품이 존재하지 않습니다."),
 	CHARGE_POINT_MAX(HttpStatus.BAD_REQUEST, "최대 충전 금액 한도를 초과하셨습니다."),
 	CHARGE_POINT_MIN(HttpStatus.BAD_REQUEST, "최소 충전 금액 이상을 충전하셔야합니다."),
-	NOT_FOUND_PRODUCT(HttpStatus.BAD_REQUEST, "해당 상품이 존재하지 않습니다.");
+	NOT_ENOUGH_POINT(HttpStatus.BAD_REQUEST, "포인트가 부족합니다."),
+	NOT_FOUND_COUPON(HttpStatus.BAD_REQUEST, "유효하지 않은 쿠폰입니다."),
+	ALREADY_USED_COUPON(HttpStatus.BAD_REQUEST, "이미 사용한 쿠폰입니다."),
+	EXPIRED_COUPON(HttpStatus.BAD_REQUEST, "만료된 쿠폰입니다."),
+	NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "유효하지 않은 사용자입니다.");
 
 	private final HttpStatus errorCode;
 	private final String message;
