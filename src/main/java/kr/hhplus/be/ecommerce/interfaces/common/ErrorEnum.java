a6b5bf3bf8223df48b60b3d3fa982ef986b0c724
khs2973 <h1372@naver.com>
2025-04-25 04:40:59 +0900

    Feat: 쿠폰 관련 enum 추가

--- src/main/java/kr/hhplus/be/ecommerce/interfaces/common/ErrorEnum.java
+++ src/main/java/kr/hhplus/be/ecommerce/interfaces/common/ErrorEnum.java
@@ -17,6 +17,7 @@ public enum ErrorEnum {
 	NOT_FOUND_COUPON(HttpStatus.BAD_REQUEST, "유효하지 않은 쿠폰입니다."),
 	ALREADY_USED_COUPON(HttpStatus.BAD_REQUEST, "이미 사용한 쿠폰입니다."),
 	EXPIRED_COUPON(HttpStatus.BAD_REQUEST, "만료된 쿠폰입니다."),
+	COUPON_STOCK_EMPTY(HttpStatus.BAD_REQUEST, "쿠폰이 마감되었습니다."),
 	NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "유효하지 않은 사용자입니다.");
 
 	private final HttpStatus errorCode;
